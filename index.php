36b0d058636efb43ef16b00133f2ae71ed663256
s2_chuyendoitiente <anhdunngcoi@gmail.com>
2018-10-24 15:13:40 +0700

    add files

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Current server time</title>
+</head>
+<body>
+<h1> Current Server Time is : <?php echo date('Y-M-d h:m:s'); ?> </h1>
+</body>
+</html>
